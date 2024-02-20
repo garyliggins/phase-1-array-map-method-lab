e0c02455f33af656132e167f58b5f7c319ca0167
gary <garyliggins@yahoo.com>
2024-02-20 15:42:44 -0800

    complete

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  });
 }
+
+// Test the function
+console.log(titleCased());
